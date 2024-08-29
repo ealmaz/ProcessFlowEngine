db1faaecdd16d4345829311be1126afb4446ac93
mshermamatov <mshermamatov@nurtelecom.kg>
2024-08-29 15:27:51 +0600

    ALGA-1474_change_foreign_password_link

--- processFlow/src/main/java/kg/devcats/processflow/ProcessFlowConfigurator.kt
+++ processFlow/src/main/java/kg/devcats/processflow/ProcessFlowConfigurator.kt
@@ -4,7 +4,7 @@ object ProcessFlowConfigurator {
 
     var selfieInstructionUrlResolver: (() -> String) = { "https://minio.o.kg/lkab/joy/light/selfi_full.png" }
     var simpleSelfieInstructionUrlResolver: (() -> String) = { "https://minio.o.kg/media-service/light/selfie.png" }
-    var foreignPasswordInstructionUrlResolver: (() -> String) = { "https://minio.o.kg/media-service/light/selfie.png" }
+    var foreignPasswordInstructionUrlResolver: (() -> String) = { "https://minio.o.kg/media-service/dark/passport.png" }
 
     var recognizerTimeoutLimit = 20
     var recognizerTimeoutMills = 20000L
