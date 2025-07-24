a0b5f168ddf4f2befce78c7992d1784006868fae
ealmaz <ealmaz@devcats.kg>
2025-07-24 12:03:33 +0600

    ProcessFlowScreen: Add refresh toolbar state callback

--- processFlow/src/main/java/kg/devcats/processflow/base/process/ProcessFlowScreen.kt
+++ processFlow/src/main/java/kg/devcats/processflow/base/process/ProcessFlowScreen.kt
@@ -9,6 +9,7 @@ interface ProcessFlowScreen {
     fun handleBackPress(): BackPressHandleState = BackPressHandleState.NOT_HANDLE
     fun handleShowLoading(isLoading: Boolean): Boolean = false
     fun handleMultipleFileLoaderContentType(loadedType: String): String? = null
+    fun refreshToolbarState() { }
 }
 
 enum class BackPressHandleState {
